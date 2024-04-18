8dffb3b845795af798c7b2467c1a71f136d88091
yasssin <yasssin@gmail.com>
2024-04-18 22:06:22 +0100

    working on add book

--- backend/book_api/routes/Book.js
+++ backend/book_api/routes/Book.js
@@ -25,27 +25,6 @@ router.get("/:id", async (req, res) => {
 
   return res.status(200).json(book);
 });
-router.get("/", async (req, res) => {
-  try {
-    const books = await Book.find();
-    return res.status(200).json(books);
-  } catch (error) {
-    return res.status(500).json({ error: "Books not found" });
-  }
-});
-
-router.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  const loan = req.body;
-
-  if (!mongoose.Types.ObjectId.isValid(id))
-    return res.status(400).json({ error: "Book id is not valid" });
-
-  const book = await Book.findOne({ _id: id });
-  if (!book) return res.status(404).json({ error: "Book not found" });
-
-  return res.status(200).json(book);
-});
 
 router.post("/", async (req, res) => {
   const newBook = req.body;
@@ -82,7 +61,6 @@ router.post("/", async (req, res) => {
       res.status(500).json({ error: "Internal server error" });
     }
   } catch (error) {
-    // Handle error
     res.status(500).json({ error: "Could not create book" });
   }
 });
