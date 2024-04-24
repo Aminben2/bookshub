eaccdbe0a3589ccf2d156bf2df639b6a1a1f9537
Aminben2 <aminbenomar454@gmail.com>
2024-04-24 15:05:14 +0100

    funxtion added in broker

--- backend/book_api/utils/broker.js
+++ backend/book_api/utils/broker.js
@@ -1,4 +1,4 @@
- import { connect } from "amqplib";
+import { connect } from "amqplib";
 
 export const sendMessageToQueue = async (queue, messageContent) => {
   try {
@@ -9,10 +9,41 @@ export const sendMessageToQueue = async (queue, messageContent) => {
       durable: true,
     });
 
-    channel.sendToQueue(queue, Buffer.from(messageContent));
+    channel.sendToQueue(queue, Buffer.from(JSON.stringify(messageContent)));
 
-    console.log(`Message sent to RabbitMQ queue '${queue}': ${messageContent}`);
+    console.log(
+      `Message sent to RabbitMQ queue '${queue}': ${messageContent}`
+    );
   } catch (error) {
     console.error("Error sending message to RabbitMQ:", error);
+  }
+};
+
+export const receiveMessageFromQueue = async (queue) => {
+  try {
+    const connection = await connect("amqp://localhost:5672");
+    const channel = await connection.createChannel();
+
+    await channel.assertQueue(queue, {
+      durable: true,
+    });
+
+    console.log(`Waiting for messages in RabbitMQ queue '${queue}'...`);
+
+    return new Promise((resolve, reject) => {
+      channel.consume(queue, (message) => {
+        if (message !== null) {
+          console.log(
+            `Received message from RabbitMQ queue '${queue}': ${message.content.toString()}`
+          );
+          const parsedData = JSON.parse(message.content.toString());
+          channel.ack(message);
+          resolve(parsedData);
+        }
+      });
+    });
+  } catch (error) {
+    console.error("Error receiving message from RabbitMQ:", error);
+    throw error;
   }
 };
